c58159396172350ae5a2b9d601de5613a393bfa2
anilpan111 <84122191+anilpan111@users.noreply.github.com>
2024-11-17 12:08:54 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
   return (
     <div className=' text-colorFour flex w-80 py-2 rounded-3xl justify-center  h-8 scroll-smooth'>
     <ul className='flex justify-between w-full h-full items-center px-4'>
-      <li className='flex-1 text-center border-r-2 cursor-pointer hover:text-xl hover:font-bold ease-in-out duration-200'><a href="#home" className=''>Home</a></li>
+      <li className='flex-1 text-center border-r-2 cursor-pointer hover:text-xl hover:font-bold ease-in-out duration-200'><a href="#home" className=''>Anil</a></li>
       <li className='flex-1 text-center border-r-2 cursor-pointer hover:text-xl hover:font-bold ease-in-out duration-200'><a href="#about">About</a></li>
       <li className='flex-1 text-center border-r-2 cursor-pointer hover:text-xl hover:font-bold ease-in-out duration-200'><a href="#projects">Projects</a></li>
       <li className='flex-1 text-center cursor-pointer hover:text-xl hover:font-bold ease-in-out duration-200'><a href="#contact">Contact</a></li>
